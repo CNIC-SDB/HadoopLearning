a47a136d4a01cfc0de61959c781d9823e3432aa9
liuang <liuang@cnic.cn>
2016-09-11 19:58:44 +0800

    解决Maven引入错误

--- wordcount/src/main/java/cn/la/hadoop/WordCount.java
+++ wordcount/src/main/java/cn/la/hadoop/WordCount.java
@@ -1,5 +1,19 @@
 package cn.la.hadoop;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WordCount {
 	public static class TokenizerMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
 		private Text word=new Text();
